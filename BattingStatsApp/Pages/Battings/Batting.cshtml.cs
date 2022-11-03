8c9484c704ab53afd324d1a505c30cfe4bdb75c1
Bob C <bobc@4bcx.com>
2022-11-03 14:21:33 -0400

    Pitching working and sortable

--- BattingStatsApp/Pages/Battings/Batting.cshtml.cs
+++ BattingStatsApp/Pages/Battings/Batting.cshtml.cs
@@ -25,43 +25,56 @@ public class BattingModel : PageModel
 
    public List<BattingUI> Batting { get; set; } = new();
    [BindProperty(SupportsGet = true)] public int CurrentYr { get; set; }
-   [BindProperty(SupportsGet = true)] public string CurrentLg { get; set; } 
-   public string CurrentSort { get; set; }
-   public async Task OnGetAsync(int yr=2021, string lg="Both")
+   [BindProperty(SupportsGet = true)] public string CurrentLg { get; set; }
+   [BindProperty(SupportsGet = true)] public string CurrentSort { get; set; }
+
+   public async Task OnGetAsync(int yr=2022, string lg="Both", string sort="")
    {
       _logger.LogInformation("In OnGetAsync");
 
       CurrentYr = yr;
       CurrentLg = lg;
+      CurrentSort = sort;
 
+      //try {
+         IQueryable<BattingUI> battingIQ =
+            from b in _context.Battings
+            from m in _context.Masters
+            where
+               b.PlayerId == m.PlayerId &&
+               (lg == b.LgId ||
+               (lg == "Both" && (b.LgId == "NL" || b.LgId == "AL"))) &&
+               b.YearId == yr &&
+               (b.Ab >= 250 || (CurrentYr == 2020 && b.Ab >= 100))
+            select new BattingUI() {
+               PlayerId = m.PlayerId,
+               PlayerName = m.NameLast + ", " + m.NameFirst + ", " + b.TeamId,
+               Pa = (int)b.Pa!,
+               Ab = b.Ab,
+               R = b.R,
+               H = b.H,
+               Rbi = b.Rbi,
+               Hr = b.Hr,
+               Bb = b.Bb,
+               Avg = Round(b.Ab > 0 ? (double)b.H / (double)b.Ab : 0.0, 3),
+               OBP = Round(b.Ab > 0 ? (double)(b.H + (int)b.Bb!) / (double)(b.Ab + (int)b.Bb) : 0.0, 3),
+               Slg = Round(b.Ab > 0 ? (double)(b.H + b._2b + 2 * b._3b + 3 * b.Hr) / (double)b.Ab : 0.0, 3)
+            };
 
-      IQueryable<BattingUI> battingIQ =
-         from b in _context.Battings
-         from m in _context.Masters
-         where
-            b.PlayerId == m.PlayerId && 
-            (lg == b.LgId || 
-            (lg == "Both" && (b.LgId == "NL" || b.LgId == "AL"))) && 
-            b.YearId == yr &&
-            (b.Ab >= 250 || (CurrentYr == 2020 && b.Ab >= 100))
-         select new BattingUI() {
-            PlayerId = m.PlayerId,
-            PlayerName = $"{m.NameLast}, {m.NameFirst}, {b.TeamId}",
-            Pa = (int)b.Pa!,
-            Ab = b.Ab,
-            R = b.R,
-            H = b.H,
-            Rbi = b.Rbi,
-            Hr = b.Hr,
-            Bb = b.Bb,
-            Avg = Round(b.Ab > 0 ? (double)b.H / (double)b.Ab : 0.0, 3),
-            OBP = Round(b.Ab > 0 ? (double)(b.H + (int)b.Bb!) / (double)(b.Ab + (int)b.Bb) : 0.0, 3),
-            Slg = Round(b.Ab > 0 ? (double)(b.H + b._2b + 2 * b._3b + 3 * b.Hr) / (double)b.Ab : 0.0, 3)
+         Batting = CurrentSort switch {
+           "Name"=> await battingIQ.OrderBy(e => e.PlayerName).ToListAsync(),
+           "Avg" => await battingIQ.OrderByDescending(e => e.Avg).ToListAsync(),
+            "OPS" => await battingIQ.OrderByDescending(e => e.OBP + e.Slg).ToListAsync(),
+            "H"   => await battingIQ.OrderByDescending(e => e.H).ToListAsync(),
+            "Ab"  => await battingIQ.OrderByDescending(e => e.Ab).ToListAsync(),
+            "Hr"  => await battingIQ.OrderByDescending(e => e.Hr).ToListAsync(),
+            "Rbi" => await battingIQ.OrderByDescending(e => e.Rbi).ToListAsync(),
+            _ => await battingIQ.OrderByDescending(e => e.OBP + e.Slg).ToListAsync()
          };
 
-
-         Batting = await battingIQ.OrderByDescending(e => e.OBP + e.Slg).ToListAsync();
+         //Batting = await battingIQ.OrderByDescending(e => e.OBP + e.Slg).ToListAsync();
          Batting.ForEach(e => e.OPS = Round(e.OBP + e.Slg, 3));
+      
    }
 
 }
--- BattingStatsApp/Pages/Battings/Pitching.cshtml.cs
+++ BattingStatsApp/Pages/Battings/Pitching.cshtml.cs
@@ -20,15 +20,16 @@ namespace BattingStatsApp.Pages.Battings
       public List<PitchingUI> Pitching { get; set; } = new();
       [BindProperty(SupportsGet = true)] public int CurrentYr { get; set; }
       [BindProperty(SupportsGet = true)] public string CurrentLg { get; set; }
-      public string CurrentSort { get; set; }
+      [BindProperty(SupportsGet = true)] public string CurrentSort { get; set; }
 
 
-      public async Task OnGetAsync(int yr = 2021, string lg = "Both")
+      public async Task OnGetAsync(int yr = 2022, string lg = "Both", string sort = "")
       {
          _logger.LogInformation("In OnGetAsync");
 
          CurrentYr = yr;
          CurrentLg = lg;
+         CurrentSort = sort;
 
          /* G W L Sv IP So Era Whip */
 
@@ -40,11 +41,11 @@ namespace BattingStatsApp.Pages.Battings
                (lg == p.LgId ||
                (lg == "Both" && (p.LgId == "NL" || p.LgId == "AL"))) &&
                p.YearId == yr &&
-               (p.Ipouts >= 150 || 
+               (p.Ipouts >= 150 ||
                (CurrentYr == 2020 && p.Ipouts >= 60))
             select new PitchingUI() {
                PlayerId = m.PlayerId,
-               PlayerName = $"{m.NameLast}, {m.NameFirst}, {p.TeamId}",
+               PlayerName = m.NameLast + ", " + m.NameFirst + ", " + p.TeamId,
                G = p.G,
                W = p.W,
                L = p.L,
@@ -52,17 +53,27 @@ namespace BattingStatsApp.Pages.Battings
                So = p.So,
                Bb = p.Bb,
                Ipouts = p.Ipouts,
+               Ip = 0.0,
                H = p.H,
-               //Ip = Round(p.Ipouts / 3.0, 1),
-               Era = Round(p.Ipouts > 0 ? p.Er / (p.Ipouts / 3.0) * 9.0 : 0.0, 2)
+               Era = Round(p.Ipouts > 0 ? p.Er / (p.Ipouts / 3.0) * 9.0 : 0.0, 2),
+               Whip = Round(p.Ipouts > 0 ? (double)(p.H + p.Bb) / (double)(p.Ipouts / 3.0) : 0.0, 3)
             };
 
+         Pitching = await pitchingIQ.ToListAsync();
+         Pitching.ForEach(e => e.Ip = e.IpDisplay);
 
-         Pitching = await pitchingIQ.OrderBy(e => e.Era).ToListAsync();
-         Pitching.ForEach(p => {
-            p.Whip = Round(p.Ipouts > 0 ? (double)(p.H + p.Bb) / (double)(p.Ipouts / 3.0) : 0.0, 3);
-            p.Ip = p.IpDisplay;
-         });
+         Pitching = CurrentSort switch {
+            "Name" => Pitching.OrderBy(e => e.PlayerName).ToList(),
+            "Ip" => Pitching.OrderByDescending(e => e.Ip).ToList(),
+            "G" => Pitching.OrderByDescending(e => e.G).ToList(),
+            "W" => Pitching.OrderByDescending(e => e.W).ToList(),
+            "L" => Pitching.OrderByDescending(e => e.L).ToList(),
+            "Sv" => Pitching.OrderByDescending(e => e.Sv).ToList(),
+            "Era" => Pitching.OrderBy(e => e.Era).ToList(),
+            "Whip" => Pitching.OrderBy(e => e.Whip).ToList(),
+            _ => Pitching.OrderBy(e => e.Era).ToList()
+         };
+
    }
 }
 
